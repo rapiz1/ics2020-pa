8ed64489f671dd75c12b4ffc59a73a9534437f8f
tracer-ics2020 <tracer@njuics.org>
2021-02-16 17:56:41 +0800

    >  compile 191220000    Static hostname: sig    Pretty hostname: Sig          Icon name: computer-laptop            Chassis: laptop         Machine ID: 714f0b96cab041a18dc9e52b7d9174e4            Boot ID: 12edc283dfc346afbcac9a1c09623d3e   Operating System: Arch Linux             Kernel: Linux 5.11.0-rc7-next-20210210-dirty       Architecture: x86-64  17:56:42 up 3 days, 18:03,  1 user,  load average: 1.52, 1.88, 1.89

--- nemu/src/isa/x86/exec/exec.c
+++ nemu/src/isa/x86/exec/exec.c
@@ -137,7 +137,7 @@ again:
     IDEX(0x56, r, push)
     IDEX(0x57, r, push)
     IDEXW(0x68, I, push, 4)
-    IDEXW(0x31, G2E, xor, 4)
+    IDEX(0x31, G2E, xor)
     EX(0xc3, ret)
   case 0x66: s->isa.is_operand_size_16 = true; goto again;
   default: exec_inv(s);
