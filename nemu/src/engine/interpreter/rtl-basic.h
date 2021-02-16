0997af42f4ded9765bd6f0a31ef4c8e34ba180af
tracer-ics2020 <tracer@njuics.org>
2021-02-16 23:27:03 +0800

    >  compile 191220000    Static hostname: sig    Pretty hostname: Sig          Icon name: computer-laptop            Chassis: laptop         Machine ID: 714f0b96cab041a18dc9e52b7d9174e4            Boot ID: 12edc283dfc346afbcac9a1c09623d3e   Operating System: Arch Linux             Kernel: Linux 5.11.0-rc7-next-20210210-dirty       Architecture: x86-64  23:27:03 up 3 days, 23:34,  1 user,  load average: 2.90, 2.20, 2.42

--- nemu/src/engine/interpreter/rtl-basic.h
+++ nemu/src/engine/interpreter/rtl-basic.h
@@ -86,11 +86,11 @@ static inline def_rtl(idiv64_r, rtlreg_t* dest,
 
 static inline def_rtl(lm, rtlreg_t *dest, const rtlreg_t* addr, word_t offset, int len) {
   *dest = vaddr_read(*addr + offset, len);
-  Log("read %x from 0x%08x", *dest, *addr + offset);
+  //Log("read %x from 0x%08x", *dest, *addr + offset);
 }
 
 static inline def_rtl(sm, const rtlreg_t* addr, word_t offset, const rtlreg_t* src1, int len) {
-  Log("write %x to 0x%08x", *src1, *addr + offset);
+  //Log("write %x to 0x%08x", *src1, *addr + offset);
   vaddr_write(*addr + offset, *src1, len);
 }
 
