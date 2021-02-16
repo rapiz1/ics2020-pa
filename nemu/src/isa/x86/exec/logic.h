78c2874b3568088109d1004079ee2248c4d139d9
tracer-ics2020 <tracer@njuics.org>
2021-02-16 17:33:39 +0800

    >  compile 191220000    Static hostname: sig    Pretty hostname: Sig          Icon name: computer-laptop            Chassis: laptop         Machine ID: 714f0b96cab041a18dc9e52b7d9174e4            Boot ID: 12edc283dfc346afbcac9a1c09623d3e   Operating System: Arch Linux             Kernel: Linux 5.11.0-rc7-next-20210210-dirty       Architecture: x86-64  17:33:39 up 3 days, 17:40,  1 user,  load average: 3.95, 2.78, 2.36

--- nemu/src/isa/x86/exec/logic.h
+++ nemu/src/isa/x86/exec/logic.h
@@ -7,19 +7,19 @@ static inline def_EHelper(test) {
 
 static inline def_EHelper(and) {
   //TODO();
-  rtl_and(s, s->dest.preg, s->dest.preg, &s->src1.reg);
+  rtl_and(s, s->dest.preg, s->dest.preg, s->src1.preg);
   print_asm_template2(and);
 }
 
 static inline def_EHelper(xor) {
   //TODO();
-  rtl_xor(s, s->dest.preg, s->dest.preg, &s->src1.reg);
+  rtl_xor(s, s->dest.preg, s->dest.preg, s->src1.preg);
   print_asm_template2(xor);
 }
 
 static inline def_EHelper(or) {
   //TODO();
-  rtl_or(s, s->dest.preg, s->dest.preg, &s->src1.reg);
+  rtl_or(s, s->dest.preg, s->dest.preg, s->src1.preg);
   print_asm_template2(or);
 }
 
