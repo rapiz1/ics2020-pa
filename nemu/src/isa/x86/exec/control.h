c617a3979b0a3170e022817a0526e5abd93ecfc6
tracer-ics2020 <tracer@njuics.org>
2021-02-16 22:35:33 +0800

    >  compile 191220000    Static hostname: sig    Pretty hostname: Sig          Icon name: computer-laptop            Chassis: laptop         Machine ID: 714f0b96cab041a18dc9e52b7d9174e4            Boot ID: 12edc283dfc346afbcac9a1c09623d3e   Operating System: Arch Linux             Kernel: Linux 5.11.0-rc7-next-20210210-dirty       Architecture: x86-64  22:35:33 up 3 days, 22:42,  1 user,  load average: 1.85, 2.63, 2.54

--- nemu/src/isa/x86/exec/control.h
+++ nemu/src/isa/x86/exec/control.h
@@ -26,6 +26,7 @@ static inline def_EHelper(call) {
   // the target address is calculated at the decode stage
   // TODO();
   rtl_push(s, &s->seq_pc);
+  rtl_j(s, s->jmp_pc);
   print_asm("call %x", s->jmp_pc);
 }
 
--- nemu/src/isa/x86/local-include/decode.h
+++ nemu/src/isa/x86/local-include/decode.h
@@ -305,5 +305,4 @@ static inline def_DHelper(call) {
   s->src1.width = 4;
   decode_op_I(s, &s->src1, false);
   s->jmp_pc = s->src1.imm + s->seq_pc;
-  s->is_jmp = true;
 }
