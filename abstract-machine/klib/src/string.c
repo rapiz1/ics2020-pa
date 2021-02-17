bc5110a6183ef9f88c571400961e0be9cdc87a46
tracer-ics2020 <tracer@njuics.org>
2021-02-17 19:08:33 +0800

    >  run 191220000    Static hostname: sig    Pretty hostname: Sig          Icon name: computer-laptop            Chassis: laptop         Machine ID: 714f0b96cab041a18dc9e52b7d9174e4            Boot ID: 12edc283dfc346afbcac9a1c09623d3e   Operating System: Arch Linux             Kernel: Linux 5.11.0-rc7-next-20210210-dirty       Architecture: x86-64  19:08:33 up 4 days, 19:15,  1 user,  load average: 2.10, 1.55, 1.58

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -28,7 +28,8 @@ int strcmp(const char* s1, const char* s2) {
     if (*s1 != *s2)
       return *s1 - *s2;
   }
-  return *s1 == 0 && *s2 == 0;
+  if (!*s1 && !*s2) return 0;
+  else return *s1 - *s2;
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
