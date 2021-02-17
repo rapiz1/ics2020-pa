eb9f4c9077a0bf1837cb05f7138986da1b6eeaf0
tracer-ics2020 <tracer@njuics.org>
2021-02-17 18:49:07 +0800

    >  run 191220000    Static hostname: sig    Pretty hostname: Sig          Icon name: computer-laptop            Chassis: laptop         Machine ID: 714f0b96cab041a18dc9e52b7d9174e4            Boot ID: 12edc283dfc346afbcac9a1c09623d3e   Operating System: Arch Linux             Kernel: Linux 5.11.0-rc7-next-20210210-dirty       Architecture: x86-64  18:49:07 up 4 days, 18:56,  1 user,  load average: 2.61, 1.96, 1.86

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -24,8 +24,11 @@ char* strcat(char* dst, const char* src) {
 }
 
 int strcmp(const char* s1, const char* s2) {
-  assert(0);
-  return 0;
+  for (;*s1 && *s2; s1++, s2++) {
+    if (*s1 != *s2)
+      return *s1 - *s2;
+  }
+  return *s1 == 0 && *s2 == 0;
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
@@ -49,7 +52,10 @@ void* memcpy(void* out, const void* in, size_t n) {
 }
 
 int memcmp(const void* s1, const void* s2, size_t n) {
-  assert(0);
+  const char *c1 = s1, *c2 = s2;
+  for (int i = 0; i < n; i++) {
+    if (c1[i] != c2[i]) return c1[i] - c2[i];
+  }
   return 0;
 }
 
