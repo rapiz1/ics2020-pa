0a88feee685ec1df60e6efbf0632e40e46973d54
tracer-ics2020 <tracer@njuics.org>
2021-02-17 18:38:16 +0800

    >  run 191220000    Static hostname: sig    Pretty hostname: Sig          Icon name: computer-laptop            Chassis: laptop         Machine ID: 714f0b96cab041a18dc9e52b7d9174e4            Boot ID: 12edc283dfc346afbcac9a1c09623d3e   Operating System: Arch Linux             Kernel: Linux 5.11.0-rc7-next-20210210-dirty       Architecture: x86-64  18:38:16 up 4 days, 18:45,  1 user,  load average: 1.58, 1.83, 1.81

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -9,38 +9,47 @@ size_t strlen(const char *s) {
 }
 
 char *strcpy(char* dst,const char* src) {
+  assert(0);
   return NULL;
 }
 
 char* strncpy(char* dst, const char* src, size_t n) {
+  assert(0);
   return NULL;
 }
 
 char* strcat(char* dst, const char* src) {
+  assert(0);
   return NULL;
 }
 
 int strcmp(const char* s1, const char* s2) {
+  assert(0);
   return 0;
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
+  assert(0);
   return 0;
 }
 
 void* memset(void* v,int c,size_t n) {
+  assert(0);
   return NULL;
 }
 
 void* memmove(void* dst,const void* src,size_t n) {
+  assert(0);
   return NULL;
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
+  assert(0);
   return NULL;
 }
 
 int memcmp(const void* s1, const void* s2, size_t n) {
+  assert(0);
   return 0;
 }
 
