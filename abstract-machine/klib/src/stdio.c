180645b9e7bc94d43ca7b9575931177ccc2d4c88
tracer-ics2020 <tracer@njuics.org>
2021-02-17 20:16:46 +0800

    >  run 191220000    Static hostname: sig    Pretty hostname: Sig          Icon name: computer-laptop            Chassis: laptop         Machine ID: 714f0b96cab041a18dc9e52b7d9174e4            Boot ID: 12edc283dfc346afbcac9a1c09623d3e   Operating System: Arch Linux             Kernel: Linux 5.11.0-rc7-next-20210210-dirty       Architecture: x86-64  20:16:46 up 4 days, 20:23,  1 user,  load average: 2.21, 2.52, 2.42

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -25,9 +25,10 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
         while (*s)
           *out++ = *s++;
       } else if (type == 'd') {
-        char buf[sizeof(int)];
+        char buf[sizeof(int)*8];
+        int buf_len = sizeof(int)*8;
         int x = va_arg(ap, int); 
-        for (int i = 0; i < 32; i++) {
+        for (int i = 0; i < buf_len; i++) {
           buf[i] = x%10 + '0';
           x /= 10;
           if (x == 0) {
