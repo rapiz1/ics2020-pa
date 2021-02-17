2d8fd089be9325e85e0db6e124a7a8ec37a11842
tracer-ics2020 <tracer@njuics.org>
2021-02-17 19:30:17 +0800

    >  run 191220000    Static hostname: sig    Pretty hostname: Sig          Icon name: computer-laptop            Chassis: laptop         Machine ID: 714f0b96cab041a18dc9e52b7d9174e4            Boot ID: 12edc283dfc346afbcac9a1c09623d3e   Operating System: Arch Linux             Kernel: Linux 5.11.0-rc7-next-20210210-dirty       Architecture: x86-64  19:30:17 up 4 days, 19:37,  1 user,  load average: 2.23, 2.09, 2.12

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -4,23 +4,33 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  assert(0);
+  int n = 0;
+  for (;*s; s++, n++);
   return 0;
 }
 
 char *strcpy(char* dst,const char* src) {
-  assert(0);
-  return NULL;
+  for (int i = 0; *src; i++)
+    dst[i] = src[i];
+  return dst;
 }
 
 char* strncpy(char* dst, const char* src, size_t n) {
-  assert(0);
-  return NULL;
+  int i;
+
+  for (i = 0; i < n && src[i] != '\0'; i++)
+    dst[i] = src[i];
+  for (;i < n; i++) dst[i] = '\0';
+  return dst;
 }
 
 char* strcat(char* dst, const char* src) {
-  assert(0);
-  return NULL;
+  int n = strlen(dst);
+  int m = strlen(src);
+  for (int i = n; i < n+m; i++)
+    dst[i] = src[i-n];
+  dst[n+m] = '\0';
+  return dst;
 }
 
 int strcmp(const char* s1, const char* s2) {
@@ -30,19 +40,29 @@ int strcmp(const char* s1, const char* s2) {
       c2 = (unsigned char) *s2++;
       if (c1 == '\0')
         return c1 - c2;
-  }
-  while (c1 == c2);
+  } while (c1 == c2);
   return c1 - c2;
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
-  assert(0);
+  unsigned char c1, c2;
+  int i = 0;
+  do {
+      c1 = (unsigned char) *s1++;
+      c2 = (unsigned char) *s2++;
+      if (c1 == '\0')
+        return c1 - c2;
+      i++;
+  } while (c1 == c2 && i < n);
   return 0;
 }
 
 void* memset(void* v,int c,size_t n) {
-  assert(0);
-  return NULL;
+  unsigned char val = (unsigned char)c;
+  for (int i = 0; i < n; i++) {
+    ((unsigned char*)v)[i] = val;
+  }
+  return v;
 }
 
 void* memmove(void* dst,const void* src,size_t n) {
@@ -51,7 +71,9 @@ void* memmove(void* dst,const void* src,size_t n) {
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
-  assert(0);
+  for (int i = 0; i < n; i++) {
+    ((unsigned char*)out)[i] = ((unsigned char*)in)[i];
+  }
   return NULL;
 }
 
