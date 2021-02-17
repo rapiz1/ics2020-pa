5ee93e35cb4f8a40d6f653184e36a3aa9ddaf823
tracer-ics2020 <tracer@njuics.org>
2021-02-17 10:09:09 +0800

    >  compile 191220000    Static hostname: sig    Pretty hostname: Sig          Icon name: computer-laptop            Chassis: laptop         Machine ID: 714f0b96cab041a18dc9e52b7d9174e4            Boot ID: 12edc283dfc346afbcac9a1c09623d3e   Operating System: Arch Linux             Kernel: Linux 5.11.0-rc7-next-20210210-dirty       Architecture: x86-64  10:09:09 up 4 days, 10:16,  1 user,  load average: 2.79, 3.61, 4.12

--- nemu/src/isa/x86/exec/exec.c
+++ nemu/src/isa/x86/exec/exec.c
@@ -92,6 +92,7 @@ static inline def_EHelper(2byte_esc) {
     IDEX(0x96, setcc_E, setcc)
     IDEX(0x97, setcc_E, setcc)
     IDEX(0xb6, E2G, movzx)
+    IDEX(0xaf, E2G, imul2)
     default: exec_inv(s);
   }
 }
