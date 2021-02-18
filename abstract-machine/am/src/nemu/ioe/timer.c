fe46d6875e062a178aa1ad184ca8cf352078b2eb
tracer-ics2020 <tracer@njuics.org>
2021-02-18 13:18:37 +0800

    >  run 191220000    Static hostname: sig    Pretty hostname: Sig          Icon name: computer-laptop            Chassis: laptop         Machine ID: 714f0b96cab041a18dc9e52b7d9174e4            Boot ID: 12edc283dfc346afbcac9a1c09623d3e   Operating System: Arch Linux             Kernel: Linux 5.11.0-rc7-next-20210210-dirty       Architecture: x86-64  13:18:37 up 5 days, 13:25,  1 user,  load average: 2.89, 1.93, 2.26

--- abstract-machine/am/src/nemu/ioe/timer.c
+++ abstract-machine/am/src/nemu/ioe/timer.c
@@ -7,7 +7,7 @@ void __am_timer_init() {
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   uptime->us = inl(0x48);
-  if (uptime->us/1000)
+  if (uptime->us%1000 == 0)
     printf("%d\n", uptime->us);
 }
 
