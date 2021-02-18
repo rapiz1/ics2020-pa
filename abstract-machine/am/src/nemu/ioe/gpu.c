a976832b1598786eb0d0ca049b5ef2b69eaf88c8
tracer-ics2020 <tracer@njuics.org>
2021-02-18 16:01:36 +0800

    >  compile 191220000    Static hostname: sig    Pretty hostname: Sig          Icon name: computer-laptop            Chassis: laptop         Machine ID: 714f0b96cab041a18dc9e52b7d9174e4            Boot ID: 12edc283dfc346afbcac9a1c09623d3e   Operating System: Arch Linux             Kernel: Linux 5.11.0-rc7-next-20210210-dirty       Architecture: x86-64  16:01:36 up 5 days, 16:08,  1 user,  load average: 1.72, 1.93, 1.86

--- abstract-machine/am/src/nemu/ioe/gpu.c
+++ abstract-machine/am/src/nemu/ioe/gpu.c
@@ -4,13 +4,21 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
+  int i;
+  uint32_t raw_value = inl(VGACTL_ADDR);
+  uint32_t w = raw_value>>16, h = raw_value&((1<<16)-1);
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (i = 0; i < w * h; i ++) fb[i] = i;
+  outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
+  uint32_t raw_value = inl(VGACTL_ADDR);
+  uint32_t w = raw_value>>16, h = raw_value&((1<<16)-1);
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = 0, .height = 0,
-    .vmemsz = 0
+    .width = w, .height = h,
+    .vmemsz = w*h 
   };
 }
 
