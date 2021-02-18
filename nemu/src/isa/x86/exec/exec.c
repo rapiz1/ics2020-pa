931a1fa9a514f005b18e703902151e86d6211dfc
tracer-ics2020 <tracer@njuics.org>
2021-02-18 16:19:37 +0800

    >  compile 191220000    Static hostname: sig    Pretty hostname: Sig          Icon name: computer-laptop            Chassis: laptop         Machine ID: 714f0b96cab041a18dc9e52b7d9174e4            Boot ID: 12edc283dfc346afbcac9a1c09623d3e   Operating System: Arch Linux             Kernel: Linux 5.11.0-rc7-next-20210210-dirty       Architecture: x86-64  16:19:37 up 5 days, 16:26,  1 user,  load average: 4.33, 2.79, 2.31

--- nemu/src/isa/x86/exec/exec.c
+++ nemu/src/isa/x86/exec/exec.c
@@ -235,9 +235,9 @@ again:
     IDEXW(0x12, E2G, adc, 1)
     IDEX(0x13, E2G, adc)
 
-    //IDEXW(0x18, G2E, sbb, 1)
+    IDEXW(0x18, G2E, sbb, 1)
     IDEX(0x19, G2E, sbb)
-    //IDEXW(0x1a, E2G, sbb, 1)
+    IDEXW(0x1a, E2G, sbb, 1)
     IDEX(0x1b, E2G, sbb)
 
     IDEXW(0x20, G2E, and, 1)
