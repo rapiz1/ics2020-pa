4fe5c5ab5abc549aa5e92f6c65ee97ee3b174161
tracer-ics2020 <tracer@njuics.org>
2021-02-18 12:04:19 +0800

    >  compile 191220000    Static hostname: sig    Pretty hostname: Sig          Icon name: computer-laptop            Chassis: laptop         Machine ID: 714f0b96cab041a18dc9e52b7d9174e4            Boot ID: 12edc283dfc346afbcac9a1c09623d3e   Operating System: Arch Linux             Kernel: Linux 5.11.0-rc7-next-20210210-dirty       Architecture: x86-64  12:04:19 up 5 days, 12:11,  1 user,  load average: 1.79, 1.84, 1.85

--- nemu/src/device/timer.c
+++ nemu/src/device/timer.c
@@ -15,7 +15,6 @@ static void rtc_io_handler(uint32_t offset, int len, bool is_write) {
     gettimeofday(&now, NULL);
     rtc_port_base[0] = now.tv_usec;
     rtc_port_base[1] = now.tv_sec;
-    printf("timer %d\n", *rtc_port_base);
   }
 }
 
