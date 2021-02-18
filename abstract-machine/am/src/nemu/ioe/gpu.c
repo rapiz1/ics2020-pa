099d2a91cf65f63ee1cd119b8c4998935bb70657
tracer-ics2020 <tracer@njuics.org>
2021-02-18 16:03:46 +0800

    >  run 191220000    Static hostname: sig    Pretty hostname: Sig          Icon name: computer-laptop            Chassis: laptop         Machine ID: 714f0b96cab041a18dc9e52b7d9174e4            Boot ID: 12edc283dfc346afbcac9a1c09623d3e   Operating System: Arch Linux             Kernel: Linux 5.11.0-rc7-next-20210210-dirty       Architecture: x86-64  16:03:46 up 5 days, 16:10,  1 user,  load average: 2.01, 2.02, 1.91

--- abstract-machine/am/src/nemu/ioe/gpu.c
+++ abstract-machine/am/src/nemu/ioe/gpu.c
@@ -21,6 +21,9 @@ void __am_gpu_init() {
   int h = cfg.height;  // TODO: get the correct height
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   for (i = 0; i < w * h; i ++) fb[i] = i;
+  for (int i = 0; i < w; i++) for (int j = 0; j < h; j++) {
+    fb[i*w + j] = i^j;
+  }
   outl(SYNC_ADDR, 1);
 }
 
