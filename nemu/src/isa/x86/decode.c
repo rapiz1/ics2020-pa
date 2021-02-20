3b0c1f0e27771423b326767cadeb22da95001b18
tracer-ics2020 <tracer@njuics.org>
2021-02-20 15:58:23 +0800

    >  compile 191220000    Static hostname: sig    Pretty hostname: Sig          Icon name: computer-laptop            Chassis: laptop         Machine ID: 714f0b96cab041a18dc9e52b7d9174e4            Boot ID: 6ea19b9e81a042809f03438e50309e67   Operating System: Arch Linux             Kernel: Linux 5.11.0-1-ck-skylake       Architecture: x86-64  15:58:23 up 22:16,  1 user,  load average: 2.81, 1.88, 1.81

--- nemu/src/isa/x86/decode.c
+++ nemu/src/isa/x86/decode.c
@@ -101,12 +101,12 @@ void read_ModR_M(DecodeExecState *s, Operand *rm, bool load_rm_val, Operand *reg
   ModR_M m;
   m.val = instr_fetch(&s->seq_pc, 1);
   s->isa.ext_opcode = m.opcode;
-  if (s->seq_pc == 0x1005f5)
-    TODO();
   if (reg != NULL) operand_reg(s, reg, load_reg_val, m.reg, reg->width);
   if (m.mod == 3) operand_reg(s, rm, load_rm_val, m.R_M, rm->width);
   else {
     load_addr(s, &m, rm);
+    if (s->seq_pc == 0x1005f5)
+      TODO();
     if (load_rm_val) rtl_lm(s, &rm->val, s->isa.mbase, s->isa.moff, rm->width);
     rm->preg = &rm->val;
   }
