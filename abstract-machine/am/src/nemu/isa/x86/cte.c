2b88f94a51f0fad54ec57a1596f220d134578d76
tracer-ics2020 <tracer@njuics.org>
2021-02-20 20:39:09 +0800

    >  run 191220000    Static hostname: sig    Pretty hostname: Sig          Icon name: computer-laptop            Chassis: laptop         Machine ID: 714f0b96cab041a18dc9e52b7d9174e4            Boot ID: 6ea19b9e81a042809f03438e50309e67   Operating System: Arch Linux             Kernel: Linux 5.11.0-1-ck-skylake       Architecture: x86-64  20:39:09 up 1 day,  2:57,  1 user,  load average: 2.08, 1.15, 1.15

--- abstract-machine/am/src/nemu/isa/x86/cte.c
+++ abstract-machine/am/src/nemu/isa/x86/cte.c
@@ -17,7 +17,7 @@ void __am_vecnull();
 inline static void display_context(Context *c) {
   printf("irq context at %d:\n", c);
   printf("\tcr3: %d at %d\n", c->cr3, &(c->cr3));
-  printf("\tedi: %d\t esi:%d\n", c->edi, c->esi);
+  printf("\tedi: %d\t esi:%d \t at %d\n", c->edi, c->esi, &(c->edi));
   printf("\tebp: %d\t esp:%d\n", c->ebp, c->esp);
   printf("\tebx: %d\t edx:%d\n", c->ebx, c->edx);
   printf("\tecx: %d\t eax:%d\n", c->ecx, c->eax);
