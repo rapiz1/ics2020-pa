ebcca8c91b968289720483cdeba0ea9de4413da3
tracer-ics2020 <tracer@njuics.org>
2021-02-20 15:31:09 +0800

    >  compile 191220000    Static hostname: sig    Pretty hostname: Sig          Icon name: computer-laptop            Chassis: laptop         Machine ID: 714f0b96cab041a18dc9e52b7d9174e4            Boot ID: 6ea19b9e81a042809f03438e50309e67   Operating System: Arch Linux             Kernel: Linux 5.11.0-1-ck-skylake       Architecture: x86-64  15:31:09 up 21:49,  1 user,  load average: 1.63, 2.03, 2.11

--- nemu/src/isa/x86/exec/system.h
+++ nemu/src/isa/x86/exec/system.h
@@ -9,8 +9,8 @@ void pio_write_b(ioaddr_t, uint32_t);
 
 static inline def_EHelper(lidt) {
   //TODO();
-  Log("lidt %x", *s->dest.preg);
   cpu.idtr = *s->dest.preg;
+  assert(false);
   print_asm_template1(lidt);
 }
 
