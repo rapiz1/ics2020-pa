0f1c50d544d3b8280fee7f8301a6afcd7e306032
tracer-ics2020 <tracer@njuics.org>
2021-02-21 17:10:45 +0800

    >  run 191220000    Static hostname: sig    Pretty hostname: Sig          Icon name: computer-laptop            Chassis: laptop         Machine ID: 714f0b96cab041a18dc9e52b7d9174e4            Boot ID: 1cad09ec88964c65a7e53998abede848   Operating System: Arch Linux             Kernel: Linux 5.10.16-arch1-1       Architecture: x86-64  17:10:45 up  5:51,  1 user,  load average: 3.53, 3.23, 2.81

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -6,11 +6,16 @@ static void SYS_yield(Context *c) {
   c->GPRx = 0;
 }
 
+static void SYS_exit(Context *c) {
+  halt(0);
+}
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
 
   switch (a[0]) {
+    case 0: SYS_exit(c); break;
     case 1: SYS_yield(c); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
