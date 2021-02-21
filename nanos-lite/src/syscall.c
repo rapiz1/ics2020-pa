eddc8f78adb25c7dac53a39b4af8082c48be87af
tracer-ics2020 <tracer@njuics.org>
2021-02-21 17:31:08 +0800

    >  run 191220000    Static hostname: sig    Pretty hostname: Sig          Icon name: computer-laptop            Chassis: laptop         Machine ID: 714f0b96cab041a18dc9e52b7d9174e4            Boot ID: 1cad09ec88964c65a7e53998abede848   Operating System: Arch Linux             Kernel: Linux 5.10.16-arch1-1       Architecture: x86-64  17:31:09 up  6:12,  1 user,  load average: 2.25, 1.76, 2.02

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,13 +1,26 @@
 #include <common.h>
 #include "syscall.h"
 
-static void SYS_yield(Context *c) {
+static void sys_exit(Context *c) {
+  halt(0);
+}
+
+static void sys_yield(Context *c) {
   yield();
   c->GPRx = 0;
 }
 
-static void SYS_exit(Context *c) {
-  halt(0);
+static void sys_write(Context *c) {
+  int fd = c->GPR2;
+  char *buf = (char*)c->GPR3;
+  size_t count = c->GPR4;
+  if (fd == 1 || fd == 2) {
+    for (int i = 0; i < count; i++)
+      putch(buf[i]);
+    c->GPRx = 0;
+  } else {
+    c->GPRx = 1;
+  }
 }
 
 void do_syscall(Context *c) {
@@ -15,8 +28,9 @@ void do_syscall(Context *c) {
   a[0] = c->GPR1;
 
   switch (a[0]) {
-    case 0: SYS_exit(c); break;
-    case 1: SYS_yield(c); break;
+    case SYS_exit: sys_exit(c); break;
+    case SYS_yield: sys_yield(c); break;
+    case SYS_write: sys_write(c); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
