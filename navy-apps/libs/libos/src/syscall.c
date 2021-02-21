74e51db1e3d692e2ec2f3f18b822bbc32a2dbcf0
tracer-ics2020 <tracer@njuics.org>
2021-02-21 19:50:41 +0800

    >  run 191220000    Static hostname: sig    Pretty hostname: Sig          Icon name: computer-laptop            Chassis: laptop         Machine ID: 714f0b96cab041a18dc9e52b7d9174e4            Boot ID: 1cad09ec88964c65a7e53998abede848   Operating System: Arch Linux             Kernel: Linux 5.10.16-arch1-1       Architecture: x86-64  19:50:41 up  8:31,  1 user,  load average: 1.99, 1.53, 1.66

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -64,8 +64,8 @@ int _write(int fd, void *buf, size_t count) {
 }
 
 void *_sbrk(intptr_t increment) {
-  extern int end;
-  void *old_end = (void*)&end;
+  extern void* end;
+  void *old_end = end;
   end += increment;
   int ret = _syscall_(SYS_brk, end, 0, 0);
   if (ret < 0) return (void*)-1;
