2df0004960840d4ce7399f2c34ac8e040b4a6f9f
tracer-ics2020 <tracer@njuics.org>
2021-02-22 20:55:20 +0800

    >  run 191220000    Static hostname: sig    Pretty hostname: Sig          Icon name: computer-laptop            Chassis: laptop         Machine ID: 714f0b96cab041a18dc9e52b7d9174e4            Boot ID: 1cad09ec88964c65a7e53998abede848   Operating System: Arch Linux             Kernel: Linux 5.10.16-arch1-1       Architecture: x86-64  20:55:20 up 1 day,  9:36,  1 user,  load average: 2.38, 3.02, 2.93

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -44,7 +44,7 @@ Context *schedule(Context *prev) {
 
   // always select pcb[0] as the new process
   current = &pcb[0];
-  Log("returned context at %p", current->cp);
+  Log("returned context at %d", current->cp);
 
   // then return the new context
   return current->cp;
