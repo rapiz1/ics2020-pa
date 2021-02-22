72c69f762b28ca49bf60c2e0722f49066d1a3ed2
tracer-ics2020 <tracer@njuics.org>
2021-02-22 11:04:42 +0800

    >  run 191220000    Static hostname: sig    Pretty hostname: Sig          Icon name: computer-laptop            Chassis: laptop         Machine ID: 714f0b96cab041a18dc9e52b7d9174e4            Boot ID: 1cad09ec88964c65a7e53998abede848   Operating System: Arch Linux             Kernel: Linux 5.10.16-arch1-1       Architecture: x86-64  11:04:42 up 23:45,  1 user,  load average: 1.64, 1.78, 1.97

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -1,5 +1,6 @@
 #include <NDL.h>
 #include <SDL.h>
+#include <assert.h>
 
 #define keyname(k) #k,
 
@@ -12,18 +13,11 @@ int SDL_PushEvent(SDL_Event *ev) {
   return 0;
 }
 
-int SDL_PollEvent(SDL_Event *ev) {
-  return 0;
-}
-
-int SDL_WaitEvent(SDL_Event *event) {
+int SDL_PollEvent(SDL_Event *event) {
   if (event == NULL) return 1;
   static char buf[64];
-  int len = 0;
-  do {
-    len = NDL_PollEvent(buf, 64);
-  } while (len == 0);
-  puts(buf);
+  int len = NDL_PollEvent(buf, 64);
+  if (!len) return 0;
   if (buf[0] == 'k') {
     int keycode;
     sscanf(buf+2, "%d", &keycode);
@@ -37,6 +31,15 @@ int SDL_WaitEvent(SDL_Event *event) {
       assert(false);
     }
   }
+  return 1;
+}
+
+int SDL_WaitEvent(SDL_Event *event) {
+  if (event == NULL) return 1;
+  int ret = 0;
+  do {
+    ret = SDL_PollEvent(event);
+  } while (ret == 0);
   return 1;
 }
 
