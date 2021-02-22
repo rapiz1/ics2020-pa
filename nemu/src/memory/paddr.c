9e9ac7994210ab113cd386a60de4f91a1ef26b37
tracer-ics2020 <tracer@njuics.org>
2021-02-22 12:03:24 +0800

    >  compile 191220000    Static hostname: sig    Pretty hostname: Sig          Icon name: computer-laptop            Chassis: laptop         Machine ID: 714f0b96cab041a18dc9e52b7d9174e4            Boot ID: 1cad09ec88964c65a7e53998abede848   Operating System: Arch Linux             Kernel: Linux 5.10.16-arch1-1       Architecture: x86-64  12:03:24 up 1 day, 44 min,  1 user,  load average: 1.59, 1.84, 2.01

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -14,12 +14,14 @@ IOMap* fetch_mmio_map(paddr_t addr);
 
 void init_mem() {
 #ifndef DIFF_TEST
+/*
   srand(time(0));
   uint32_t *p = (uint32_t *)pmem;
   int i;
   for (i = 0; i < PMEM_SIZE / sizeof(p[0]); i ++) {
     p[i] = rand();
   }
+  */
 #endif
 }
 
