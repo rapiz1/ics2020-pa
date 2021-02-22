ce0438935f305db39011055f203d2d7beae1c5bd
tracer-ics2020 <tracer@njuics.org>
2021-02-22 12:58:37 +0800

    >  run 191220000    Static hostname: sig    Pretty hostname: Sig          Icon name: computer-laptop            Chassis: laptop         Machine ID: 714f0b96cab041a18dc9e52b7d9174e4            Boot ID: 1cad09ec88964c65a7e53998abede848   Operating System: Arch Linux             Kernel: Linux 5.10.16-arch1-1       Architecture: x86-64  12:58:37 up 1 day,  1:39,  1 user,  load average: 2.04, 2.17, 1.74

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -98,6 +98,7 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
 
 void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
   puts("SDL_FillRect called");
+  assert(dst->format->BytesPerPixel == 4);
   SDL_Color c = _ParseColorFromSurface(dst, color);
   SDL_Rect tmp;
   if (dstrect == NULL) {
@@ -110,6 +111,7 @@ void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
   uint32_t *pixels = malloc(sizeof(uint32_t)*n);
   for (int i = 0; i < n; i++) {
     pixels[i] = _GetVGAColor(c);
+    ((uint32_t*)dst->pixels)[i] = SDL_MapRGBA(dst->format, c.r, c.g, c.b, c.a);
   }
   NDL_DrawRect(pixels, dstrect->x, dstrect->y, dstrect->w, dstrect->h);
   free(pixels);
