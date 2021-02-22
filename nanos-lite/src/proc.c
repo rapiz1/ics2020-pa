bb2334dffbd5f3082a2d2640523b36b899d59484
tracer-ics2020 <tracer@njuics.org>
2021-02-22 22:07:46 +0800

    >  run 191220000    Static hostname: sig    Pretty hostname: Sig          Icon name: computer-laptop            Chassis: laptop         Machine ID: 714f0b96cab041a18dc9e52b7d9174e4            Boot ID: 1cad09ec88964c65a7e53998abede848   Operating System: Arch Linux             Kernel: Linux 5.10.16-arch1-1       Architecture: x86-64  22:07:46 up 1 day, 10:48,  1 user,  load average: 1.79, 1.36, 1.56

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -25,6 +25,7 @@ void context_kload(PCB *pcb, void (*entry)(void *), void *arg) {
 
 void init_proc() {
   context_kload(&pcb[0], hello_fun, (void*)1);
+  context_kload(&pcb[1], hello_fun, (void*)99);
   switch_boot_pcb();
 
   Log("Initializing processes...");
@@ -42,7 +43,9 @@ Context *schedule(Context *prev) {
   current->cp = prev;
 
   // always select pcb[0] as the new process
-  current = &pcb[0];
+  //current = &pcb[0];
+  current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
+
 
   // then return the new context
   return current->cp;
