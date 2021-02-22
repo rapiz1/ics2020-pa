2e8f9b9a43496f6b80d6f3c98f045b5c3b711535
tracer-ics2020 <tracer@njuics.org>
2021-02-22 13:16:51 +0800

    >  run 191220000    Static hostname: sig    Pretty hostname: Sig          Icon name: computer-laptop            Chassis: laptop         Machine ID: 714f0b96cab041a18dc9e52b7d9174e4            Boot ID: 1cad09ec88964c65a7e53998abede848   Operating System: Arch Linux             Kernel: Linux 5.10.16-arch1-1       Architecture: x86-64  13:16:51 up 1 day,  1:58,  1 user,  load average: 2.46, 1.57, 1.53

--- navy-apps/apps/nterm/src/main.cpp
+++ navy-apps/apps/nterm/src/main.cpp
@@ -49,6 +49,7 @@ void refresh_terminal() {
   static int flip = 0;
   uint32_t now = SDL_GetTicks();
   if (now - last > 500 || needsync) {
+    printf("tick %d\n", now);
     int x = term->cursor.x, y = term->cursor.y;
     uint32_t color = (flip ? term->foreground(x, y) : term->background(x, y));
     draw_ch(x * font->w, y * font->h, ' ', 0, color);
