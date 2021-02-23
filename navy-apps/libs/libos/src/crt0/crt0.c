ca104a4823bc271117cc43094ea9f353a444aca8
tracer-ics2020 <tracer@njuics.org>
2021-02-23 17:53:47 +0800

    >  run 191220000    Static hostname: sig    Pretty hostname: Sig          Icon name: computer-laptop            Chassis: laptop         Machine ID: 714f0b96cab041a18dc9e52b7d9174e4            Boot ID: 1a3c51f202314ebbb25f24adb2e621a4   Operating System: Arch Linux             Kernel: Linux 5.10.16-arch1-1       Architecture: x86-64  17:53:47 up  2:33,  1 user,  load average: 3.20, 2.11, 1.79

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -10,12 +10,10 @@ void call_main(uintptr_t *args) {
   int argc = (int)args;
   char **argv = (char**)&args + 1;
   environ = (char**)&args + 1 + argc + 1;
-  /*
   for (int i = 0; i < argc; i++)
     printf("argv[%d]=%s\n", i, argv[i]);
   for (int i = 0; environ[i]; i++)
     printf("env %s\n", environ[i]);
-    */
   puts("=====CRT0 END=====");
   puts("======START=======");
   exit(main(argc, argv, environ));
