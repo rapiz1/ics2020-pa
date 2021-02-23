58c823a7f6f9440fce10e0150d868e641d510c08
tracer-ics2020 <tracer@njuics.org>
2021-02-23 15:47:22 +0800

    >  run 191220000    Static hostname: sig    Pretty hostname: Sig          Icon name: computer-laptop            Chassis: laptop         Machine ID: 714f0b96cab041a18dc9e52b7d9174e4            Boot ID: 1a3c51f202314ebbb25f24adb2e621a4   Operating System: Arch Linux             Kernel: Linux 5.10.16-arch1-1       Architecture: x86-64  15:47:22 up 27 min,  1 user,  load average: 1.37, 1.20, 1.13

--- navy-apps/apps/menu/menu.cpp
+++ navy-apps/apps/menu/menu.cpp
@@ -112,6 +112,7 @@ int main(int argc, char *argv[], char *envp[]) {
       exec_argv[2] = NULL;
       clear_display();
       SDL_UpdateRect(screen, 0, 0, 0, 0);
+      printf("execve argv %s\n", exec_argv);
       execve(exec_argv[0], (char**)exec_argv, (char**)envp);
       fprintf(stderr, "\033[31m[ERROR]\033[0m Exec %s failed.\n\n", exec_argv[0]);
     } else {
