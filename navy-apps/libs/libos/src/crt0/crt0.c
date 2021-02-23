cd42671b8b55a5410ef564d34616c43944bbc124
tracer-ics2020 <tracer@njuics.org>
2021-02-23 16:06:04 +0800

    >  run 191220000    Static hostname: sig    Pretty hostname: Sig          Icon name: computer-laptop            Chassis: laptop         Machine ID: 714f0b96cab041a18dc9e52b7d9174e4            Boot ID: 1a3c51f202314ebbb25f24adb2e621a4   Operating System: Arch Linux             Kernel: Linux 5.10.16-arch1-1       Architecture: x86-64  16:06:04 up 46 min,  1 user,  load average: 3.32, 2.60, 2.00

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <assert.h>
 
 int main(int argc, char *argv[], char *envp[]);
@@ -8,6 +9,10 @@ void call_main(uintptr_t *args) {
   int argc = (int)args;
   char **argv = (char**)&args + 1;
   environ = (char**)&args + 1 + argc + 1;
+  for (int i = 0; i < argc; i++)
+    printf("argv[%d]=%s\n", i, argv[i]);
+  for (int i = 0; environ[i]; i++)
+    printf("env %s\n", environ[i]);
   exit(main(argc, argv, environ));
   assert(0);
 }
