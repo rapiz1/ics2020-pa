872e4a55100810e821e95eb87b4560bb0acbb8d5
tracer-ics2020 <tracer@njuics.org>
2021-02-23 13:18:39 +0800

    >  run 191220000    Static hostname: sig    Pretty hostname: Sig          Icon name: computer-laptop            Chassis: laptop         Machine ID: 714f0b96cab041a18dc9e52b7d9174e4            Boot ID: 1cad09ec88964c65a7e53998abede848   Operating System: Arch Linux             Kernel: Linux 5.10.16-arch1-1       Architecture: x86-64  13:18:39 up 2 days,  1:59,  1 user,  load average: 3.21, 2.05, 1.90

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -8,7 +8,7 @@ static Context* do_event(Event e, Context* c) {
   switch (e.event) {
     case EVENT_SYSCALL: /*Log("Received syscall event %d", c->GPR1);*/ do_syscall(c); break;
     case EVENT_YIELD:
-      Log("Received yield event");
+      //Log("Received yield event");
       c = schedule(c);
       break;
     default: panic("Unhandled event ID = %d", e.event);
