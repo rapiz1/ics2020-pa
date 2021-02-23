499d0738aa0d1345aae63d9db497e6fadc609442
tracer-ics2020 <tracer@njuics.org>
2021-02-23 15:28:07 +0800

    >  run 191220000    Static hostname: sig    Pretty hostname: Sig          Icon name: computer-laptop            Chassis: laptop         Machine ID: 714f0b96cab041a18dc9e52b7d9174e4            Boot ID: 1a3c51f202314ebbb25f24adb2e621a4   Operating System: Arch Linux             Kernel: Linux 5.10.16-arch1-1       Architecture: x86-64  15:28:07 up 8 min,  1 user,  load average: 2.11, 1.59, 0.87

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -81,7 +81,7 @@ void init_proc() {
   context_kload(&pcb[0], hello_fun, (void*)1);
   char *argv[] = {"/bin/nterm", "HELLO FROM NANOS-LITE", NULL};
   char *envp[] = {"HELLO=NANOS-LITE", "AUTHOR=rapiz", NULL};
-  context_uload(&pcb[1], "/bin/nterm", argv, envp);
+  context_uload(&pcb[1], "/bin/menu", argv, envp);
   switch_boot_pcb();
 
   Log("Initializing processes...");
