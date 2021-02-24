eef6b0faaa068b0577a1ec491babea466cdde52d
tracer-ics2020 <tracer@njuics.org>
2021-02-24 20:06:10 +0800

    >  compile 191220000    Static hostname: sig    Pretty hostname: Sig          Icon name: computer-laptop            Chassis: laptop         Machine ID: 714f0b96cab041a18dc9e52b7d9174e4            Boot ID: e08d5836a2034b878e6ceb4afe3966de   Operating System: Arch Linux             Kernel: Linux 5.11.1-arch1-1       Architecture: x86-64  20:06:10 up 59 min,  1 user,  load average: 3.72, 2.74, 2.35

--- nemu/src/isa/x86/mmu.c
+++ nemu/src/isa/x86/mmu.c
@@ -2,8 +2,8 @@
 #include <memory/vaddr.h>
 #include <memory/paddr.h>
 paddr_t isa_mmu_translate(vaddr_t vaddr, int type, int len) {
-  if (GET_DIR(vaddr) != GET_DIR(vaddr+len)) return MEM_RET_CROSS_PAGE;
-  if (GET_PAGE(vaddr) != GET_PAGE(vaddr+len)) return MEM_RET_CROSS_PAGE;
+  if (GET_DIR(vaddr) != GET_DIR(vaddr+len-1)) return MEM_RET_CROSS_PAGE;
+  if (GET_PAGE(vaddr) != GET_PAGE(vaddr+len-1)) return MEM_RET_CROSS_PAGE;
   paddr_t updir = cpu.cr3;
   paddr_t pde_addr = updir + sizeof(PTE)*GET_DIR(vaddr);
   PTE pde;
