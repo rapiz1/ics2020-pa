ae76fc7017a4b72584a039924cdcb19ee589f29c
tracer-ics2020 <tracer@njuics.org>
2021-02-24 20:07:02 +0800

    >  run 191220000    Static hostname: sig    Pretty hostname: Sig          Icon name: computer-laptop            Chassis: laptop         Machine ID: 714f0b96cab041a18dc9e52b7d9174e4            Boot ID: e08d5836a2034b878e6ceb4afe3966de   Operating System: Arch Linux             Kernel: Linux 5.11.1-arch1-1       Architecture: x86-64  20:07:02 up  1:00,  1 user,  load average: 3.13, 2.81, 2.40

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -12,10 +12,8 @@ void switch_boot_pcb() { current = &pcb_boot; }
 void hello_fun(void *arg) {
   int j = 1;
   while (1) {
-    /*
     Log("Hello World from Nanos-lite with arg '%d' for the %dth time!, at %d",
         (uintptr_t)arg, j, &arg);
-        */
     j++;
     yield();
   }
@@ -84,11 +82,9 @@ void context_kload(PCB *pcb, void (*entry)(void *), void *arg) {
 
 void init_proc() {
   context_kload(&pcb[0], hello_fun, (void*)1);
-  /*
   char *argv[] = {"/bin/menu", "HELLO FROM NANOS-LITE", NULL};
   char *envp[] = {"HELLO=NANOS-LITE", "AUTHOR=rapiz", NULL};
   context_uload(&pcb[1], "/bin/menu", argv, envp);
-  */
   switch_boot_pcb();
 
   Log("Initializing processes...");
@@ -106,9 +102,8 @@ Context *schedule(Context *prev) {
   current->cp = prev;
 
   // always select pcb[0] as the new process
-  current = &pcb[0];
   //current = &pcb[1];
-  //current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
+  current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
 
   // then return the new context
   return current->cp;
