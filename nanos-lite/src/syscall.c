5496d750450fca24e002c4bc8cfd5ab390f013dc
tracer-ics2020 <tracer@njuics.org>
2021-02-24 22:21:20 +0800

    >  run 191220000    Static hostname: sig    Pretty hostname: Sig          Icon name: computer-laptop            Chassis: laptop         Machine ID: 714f0b96cab041a18dc9e52b7d9174e4            Boot ID: e08d5836a2034b878e6ceb4afe3966de   Operating System: Arch Linux             Kernel: Linux 5.11.1-arch1-1       Architecture: x86-64  22:21:20 up  3:14,  1 user,  load average: 2.56, 2.12, 2.07

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -104,7 +104,7 @@ static void sys_execve(Context *c) {
 }
 
 static void sys_exit(Context *c) {
-  halt(1);
+  halt(0);
   c->GPRx = 0;
   const char *filename = "/bin/nterm";
   const char *argv[] = {"/bin/nterm", NULL};
