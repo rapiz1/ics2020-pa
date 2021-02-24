d0575bc0f78168df921bdd361d396f16c862608f
tracer-ics2020 <tracer@njuics.org>
2021-02-24 23:32:06 +0800

    >  compile 191220000    Static hostname: sig    Pretty hostname: Sig          Icon name: computer-laptop            Chassis: laptop         Machine ID: 714f0b96cab041a18dc9e52b7d9174e4            Boot ID: e08d5836a2034b878e6ceb4afe3966de   Operating System: Arch Linux             Kernel: Linux 5.11.1-arch1-1       Architecture: x86-64  23:32:06 up  4:25,  1 user,  load average: 1.12, 1.98, 2.08

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -171,10 +171,12 @@ static int cmd_help(char *args) {
 }
 
 void ui_mainloop() {
+  /*
   if (is_batch_mode()) {
     cmd_c(NULL);
     return;
   }
+  */
 
   for (char *str; (str = rl_gets()) != NULL; ) {
     char *str_end = str + strlen(str);
