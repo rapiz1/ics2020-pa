f4ea00e9e3ab0249c1539581608576ed9b9b4656
tracer-ics2020 <tracer@njuics.org>
2021-02-25 15:51:49 +0800

    >  compile 191220000    Static hostname: sig    Pretty hostname: Sig          Icon name: computer-laptop            Chassis: laptop         Machine ID: 714f0b96cab041a18dc9e52b7d9174e4            Boot ID: e08d5836a2034b878e6ceb4afe3966de   Operating System: Arch Linux             Kernel: Linux 5.11.1-arch1-1       Architecture: x86-64  15:51:49 up 20:45,  1 user,  load average: 2.22, 2.15, 1.96

--- nemu/src/isa/x86/intr.c
+++ nemu/src/isa/x86/intr.c
@@ -46,12 +46,9 @@ void raise_intr(DecodeExecState *s, uint32_t NO, vaddr_t ret_addr) {
 }
 
 void query_intr(DecodeExecState *s) {
-  //TODO();
-  /*
-  if (cpu.INTR && (cpu.eflags&FL_IF)) {
+  if (cpu.INTR && (cpu.eflags&FL_IF) && cpu.idtr.limit != 0) {
     cpu.INTR = false;
     raise_intr(s, IRQ_TIMER, s->seq_pc);
     update_pc(s);
   }
-  */
 }
