f172812d5b46632fdbaf214944e6524132bfa912
tracer-ics2020 <tracer@njuics.org>
2021-02-25 11:43:51 +0800

    >  run 191220000    Static hostname: sig    Pretty hostname: Sig          Icon name: computer-laptop            Chassis: laptop         Machine ID: 714f0b96cab041a18dc9e52b7d9174e4            Boot ID: e08d5836a2034b878e6ceb4afe3966de   Operating System: Arch Linux             Kernel: Linux 5.11.1-arch1-1       Architecture: x86-64  11:43:51 up 16:37,  1 user,  load average: 2.51, 2.39, 2.11

---
+++ navy-apps/tests/fib-test/fib.c
@@ -0,0 +1,14 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+int fib(int n) {
+	if (n == 0 || n == 1) return 1;
+	return fib(n-1) + fib(n-2);
+}
+int main() {
+	for (int i = 0; i < 10; i++) printf("%d:%d\n", i, fib(i));
+	return 0;
+}
